aec3c78882ba8b984b43127105d5eaaed253cc63
ehy0419 <2hy0419@naver.com>
2025-08-12 14:08:06 +0900

    feat. 패키지 설정 및 JpaAuditingConfig 감사설정 클래스

---
+++ src/main/java/com/chronos_0812/Chronos0812Application.java
@@ -0,0 +1,13 @@
+package com.chronos_0812;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chronos0812Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chronos0812Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chronos_0812/Chronos0812ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronos_0812;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chronos0812ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
